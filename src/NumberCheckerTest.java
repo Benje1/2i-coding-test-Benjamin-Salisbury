3f9424d454f4ea3cde489940196c8fd374438c5b
Benje1 <102314569+Benje1@users.noreply.github.com>
2022-10-27 14:37:38 +0100

    sort function works

--- src/NumberCheckerTest.java
+++ src/NumberCheckerTest.java
@@ -13,6 +13,7 @@ public class NumberCheckerTest {
     List<Integer> listOfNumbers;
     List<Integer> listOfDistinctNumbers;
     List<Integer> wrongList;
+    List<Integer> sortedList;
 
     @Before
     public void before(){
@@ -20,6 +21,7 @@ public class NumberCheckerTest {
         listOfNumbers = new ArrayList<>();
         listOfDistinctNumbers = new ArrayList<>();
         wrongList = new ArrayList<>();
+        sortedList = new ArrayList<>();
         listOfNumbers.add(1);
         listOfNumbers.add(1);
         listOfNumbers.add(3);
@@ -40,6 +42,16 @@ public class NumberCheckerTest {
         wrongList.add(1);
         wrongList.add(1);
         wrongList.add(1);
+        sortedList.add(65);
+        sortedList.add(65);
+        sortedList.add(24);
+        sortedList.add(12);
+        sortedList.add(12);
+        sortedList.add(7);
+        sortedList.add(3);
+        sortedList.add(3);
+        sortedList.add(1);
+        sortedList.add(1);
     }
 
     @Test
@@ -47,9 +59,20 @@ public class NumberCheckerTest {
         assertArrayEquals(listOfDistinctNumbers.toArray(), numberChecker.removeDuplicates(listOfNumbers).toArray());
     }
 
+    //This following test is made to fail, checking that the function does not default to passing
     @Test
     public void failTestAsNotSame(){
         assertArrayEquals(wrongList.toArray(), numberChecker.removeDuplicates(listOfNumbers).toArray());
+    }
+
+    @Test
+    public void isTheListSorted(){
+        assertArrayEquals(sortedList.toArray(), numberChecker.sortNumbersFromHighToLow(listOfNumbers).toArray());
+    }
+
+    @Test
+    public void failTestAsNotOrdered(){
+        assertArrayEquals(wrongList.toArray(), numberChecker.sortNumbersFromHighToLow(listOfNumbers).toArray());
     }
 
 }
